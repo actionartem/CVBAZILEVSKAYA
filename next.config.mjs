02496d9a2c35a5dbf69e80737b94581e95691cd4
Artem <Actionartem@gmail.com>
2026-02-01 04:18:00 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
+  // Чтобы сайт работал в подпапке GitHub Pages: /CVBAZILEVSKAYA
+  basePath: "/CVBAZILEVSKAYA",
+  assetPrefix: "/CVBAZILEVSKAYA/",
+  trailingSlash: true,
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
